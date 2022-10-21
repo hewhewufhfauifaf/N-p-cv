cd72544e8eb9bf8b0fd5028cc3a7852d7ac6bc21
hewhewufhfauifaf <116350704+hewhewufhfauifaf@users.noreply.github.com>
2022-10-21 17:20:16 +0700

    Add files via upload

---
+++ songuyentotrongmang.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int songuyento(int n){
+    for(int i=2;i<=sqrt(n);i++){
+         if(n%i==0) return 0;
+    }
+    return n>1;
+}
+int main(){
+    cout<<"chuong trinh tim so nguyen to trong 1 mang"<<endl;
+    cout<<"so phan tu cua mang la :";
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cout<<"phan tu thu "<<i<<"la :";
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(songuyento(a[i])){
+            cout<<"cac phan tu trong mang la so nguyen to la :";
+            cout<<a[i]<<endl;
+        }
+    }
+}
